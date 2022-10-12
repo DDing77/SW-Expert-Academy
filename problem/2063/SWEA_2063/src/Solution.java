9c144f19b2cf05d04a7ce33748e766f2249cac76
DDing77 <devseo2021@gmail.com>
2022-10-12 21:38:19 +0900

    SWEA_2063

---
+++ problem/2063/SWEA_2063/src/Solution.java
@@ -0,0 +1,29 @@
+/* No.2063
+   Author : DDing
+   Ref : https://swexpertacademy.com/main/main.do  */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+    static int N;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+        System.out.println(arr[N / 2]);
+    }
+}
