0fffb1accbd45532dc19804d9c2734a40a34b905
DDing77 <devseo2021@gmail.com>
2022-10-13 23:53:54 +0900

    SWEA_2027

---
+++ problem/2027/SWEA_2027/src/Solution.java
@@ -0,0 +1,19 @@
+/* No.2027
+   Author : DDing
+   Ref : https://swexpertacademy.com/main/main.do  */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("#++++\n" +
+                "+#+++\n" +
+                "++#++\n" +
+                "+++#+\n" +
+                "++++#");
+    }
+}
