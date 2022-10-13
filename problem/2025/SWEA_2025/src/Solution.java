a1eb20ef818579e983071d982435715a60918a78
DDing77 <devseo2021@gmail.com>
2022-10-14 00:03:29 +0900

    SWEA_2025

---
+++ problem/2025/SWEA_2025/src/Solution.java
@@ -0,0 +1,20 @@
+/* No.2025
+   Author : DDing
+   Ref : https://swexpertacademy.com/main/main.do  */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        System.out.println(((N + 1) * N) / 2);
+    }
+}
