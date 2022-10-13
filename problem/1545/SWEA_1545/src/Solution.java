88811fb2f757955b067b125348d4b8e764e17ed8
DDing77 <devseo2021@gmail.com>
2022-10-14 00:30:43 +0900

    SWEA_1545

---
+++ problem/1545/SWEA_1545/src/Solution.java
@@ -0,0 +1,23 @@
+/* No.1545
+   Author : DDing
+   Ref : https://swexpertacademy.com/main/main.do  */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+
+        for (int i = N; i >= 0; i--) sb.append(i + " ");
+
+        System.out.println(sb);
+    }
+}
