b82f2bb178a5aff0e07d2c900808993107ab07a2
DDing77 <devseo2021@gmail.com>
2022-10-27 01:20:46 +0900

    SWEA_12368

---
+++ problem/12368/SWEA_12368/src/Solution.java
@@ -0,0 +1,30 @@
+/* No.12368
+   Author : DDing
+   Type : Math
+   Ref : https://swexpertacademy.com/main/main.do  */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+    static int T;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            sb.append("#" + tc + " " + (A + B) % 24 + '\n');
+        }
+        System.out.print(sb);
+    }
+}
